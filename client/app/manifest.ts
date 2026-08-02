bbd0a07f57903206f265b5b4a2a55301566909d0
Newthin <newthin@users.noreply.github.com>
2026-08-02 15:18:47 +0000

    Use provided ROICARD app icon for favicon, apple, and manifest

--- client/app/manifest.ts
+++ client/app/manifest.ts
@@ -16,12 +16,12 @@ export default function manifest() {
         type: "image/png",
       },
       {
-        src: "/images/logo.png",
+        src: "/images/icon.png",
         sizes: "512x512",
         type: "image/png",
       },
       {
-        src: "/images/logo.png",
+        src: "/images/icon.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
