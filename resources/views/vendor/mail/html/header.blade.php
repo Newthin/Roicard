bfa572e9abbc166dd52eef168919b1a4e4fd2185
Newthin <newthin@users.noreply.github.com>
2026-08-03 10:17:57 +0000

    Stretch email logo wide (320px width)

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
     <a href="{{ $url }}" style="display: inline-block;">
-        <img src="{{ config('app.logo_url', rtrim(config('app.frontend_url', '/'), '/') . '/images/logo.png') }}" class="logo" alt="Roicard Logo" style="height: 130px; width: auto; max-width: 320px;">
+        <img src="{{ config('app.logo_url', rtrim(config('app.frontend_url', '/'), '/') . '/images/logo.png') }}" class="logo" alt="Roicard Logo" style="width: 320px; height: auto; max-height: 130px;">
     </a>
 </td>
 </tr>
