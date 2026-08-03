970784bcb266154557cc14b31a20cd6e82d57626
Newthin <newthin@users.noreply.github.com>
2026-08-03 10:16:06 +0000

    Enlarge email logo to 130px

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
     <a href="{{ $url }}" style="display: inline-block;">
-        <img src="{{ config('app.logo_url', rtrim(config('app.frontend_url', '/'), '/') . '/images/logo.png') }}" class="logo" alt="Roicard Logo" style="height: 110px; width: auto; max-width: 280px;">
+        <img src="{{ config('app.logo_url', rtrim(config('app.frontend_url', '/'), '/') . '/images/logo.png') }}" class="logo" alt="Roicard Logo" style="height: 130px; width: auto; max-width: 320px;">
     </a>
 </td>
 </tr>
